5414139a86096d2b87e0ee37eed5fa64fc62aff2
Nikita Elunin <letsleroyme@gmail.com>
2023-08-30 19:38:49 +0600

    tests

---
+++ test/6-old-vault-new-wever.ts
@@ -0,0 +1,89 @@
+import {EMPTY_TVM_CELL, getMetricsChange, getVaultMetrics, 
+    logMetricsChange, setupWever, ZERO_ADDRESS, setupLegacyWever} from "./utils";
+import {Address, Contract, toNano} from "locklift";
+import {expect} from "chai";
+
+// @ts-ignore
+import logger from 'mocha-logger';
+import { TestMinterBurnerAbi, VaultTokenWallet_V1Abi } from "../build/factorySource";
+
+
+describe('Transfer tokens with additional wrap', async function() {
+    this.timeout(200000);
+
+    // @ts-ignore
+    let context: ReturnType<typeof setupWever> extends Promise<infer F> ? F : never = {};
+    let minterBurner: Contract<TestMinterBurnerAbi>;
+
+    it("Setup contracts", async function () {
+        await locklift.deployments.fixture();
+
+        context = await setupLegacyWever();
+        minterBurner = await locklift.deployments.getContract<TestMinterBurnerAbi>('TestMinterBurner');
+    });
+
+    it("Call mint on new Wever, with new Vault", async function () {
+        const { aliceTokenWallet, alice, vaultTokenWallet, vault } = context;
+
+        let payload = await minterBurner.methods.buildPayload({addr: alice.address}).call().then(v => v.value0);
+
+
+       const {traceTree} = await locklift.tracing.trace(
+        minterBurner.methods.mint({
+            amount: toNano(10), 
+            to: alice.address, 
+            remainingGasTo: alice.address, 
+            payload: payload
+        }).send({from: alice.address, amount: toNano(20)}));
+
+       traceTree?.beautyPrint();
+    //    expect(traceTree).to.emit('MintCallback').withNamedArgs({payload: payload});
+    });
+
+    it("Call vault directly", async function () {
+        const { aliceTokenWallet, alice, vaultTokenWallet, vault } = context;
+
+        let payload = await minterBurner.methods.buildPayload({addr: alice.address}).call().then(v => v.value0);
+
+
+       const {traceTree} = await locklift.tracing.trace(
+        vault.methods.wrap({
+            tokens: toNano(10), 
+            owner_address: alice.address, 
+            gas_back_address: alice.address, 
+            payload: payload
+        }).send({from: alice.address, amount: toNano(40)}));
+
+       traceTree?.beautyPrint();
+    });
+
+    it("Burn from minterBurner", async function () {
+        const { aliceTokenWallet, alice, vaultTokenWallet, vault } = context;
+
+        let payload = await minterBurner.methods.buildPayload({addr: alice.address}).call().then(v => v.value0);
+        
+        let minterBurnerTW = await locklift.deployments.getContract<VaultTokenWallet_V1Abi>('MinterBurnerTokenWallet');
+
+
+       const {traceTree} = await locklift.tracing.trace(
+        aliceTokenWallet.methods.transfer({
+            amount: toNano(5), 
+            recipient: minterBurner.address, 
+            deployWalletValue: 0, 
+            remainingGasTo: minterBurner.address, 
+            notify: true,
+            payload: payload
+        }).send({from: alice.address, amount: toNano(10)}));
+
+       traceTree?.beautyPrint();
+       expect(traceTree).to.emit("BurnCallback").withNamedArgs({payload: payload});
+        expect(traceTree).to.call("acceptBurn").withNamedArgs({
+            amount: toNano(5), 
+            walletOwner: vault.address, 
+            callbackto: minterBurner.address, 
+            remainingGasTo: minterBurnerTW.address, 
+            payload: payload
+        });
+    });
+
+})
